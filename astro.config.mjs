aefc03f5490f63e9365ec3fb2b4b90e4242a244d
Jesstern Rays <jsstrn@users.noreply.github.com>
2025-10-02 23:01:31 +0800

    chore: update site title and description

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Learn with Jesstern',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/learn-with-jesstern' }],
 			sidebar: [
 				{
 					label: 'Guides',

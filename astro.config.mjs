4e66dbadbc10ff9b9a8c1d16ad8b6e9c157f3e7c
Jesstern Rays <jsstrn@users.noreply.github.com>
2025-10-30 15:56:30 +0800

    Update favicon path

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "Cloud Engineering",
-      favicon: "favicon.ico",
+      favicon: "./favicon.ico",
       social: [
         {
           icon: "github",
